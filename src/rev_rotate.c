54e770b552b72f6907e34bff74a03522cbd657e2
Pierre Lamusse <p.lamusse@hotmail.fr>
2018-04-26 18:49:12 +0200

    checker done

--- src/rev_rotate.c
+++ src/rev_rotate.c
@@ -6,7 +6,7 @@
 /*   By: plamusse <plamusse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/27 12:15:29 by plamusse          #+#    #+#             */
-/*   Updated: 2018/04/25 20:17:44 by plamusse         ###   ########.fr       */
+/*   Updated: 2018/04/26 18:38:52 by plamusse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,14 @@
 
 void	rev_rotate_a(t_list **a, t_list **b)
 {
-	t_list	**tmp;
-
+	(void)b;
 	*a = (*a)->prev;
-	tmp = b;
 }
 
 void	rev_rotate_b(t_list **a, t_list **b)
 {
-	t_list	**tmp;
-
+	(void)a;
 	*b = (*b)->prev;
-	tmp = a;
 }
 
 void	rev_rotate_ab(t_list **a, t_list **b)
--- src/rotate.c
+++ src/rotate.c
@@ -6,7 +6,7 @@
 /*   By: plamusse <plamusse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/26 16:39:50 by plamusse          #+#    #+#             */
-/*   Updated: 2018/04/25 20:18:08 by plamusse         ###   ########.fr       */
+/*   Updated: 2018/04/26 18:38:11 by plamusse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,14 @@
 
 void	rotate_a(t_list **a, t_list **b)
 {
-	t_list	**tmp;
-
+	(void)b;
 	*a = (*a)->next;
-	tmp = b;
 }
 
 void	rotate_b(t_list **a, t_list **b)
 {
-	t_list	**tmp;
-
+	(void)a;
 	*b = (*b)->next;
-	tmp = a;
 }
 
 void	rotate_ab(t_list **a, t_list **b)
